b5d45b39e02066fa79f42b5f4219afd4a5b8929a
Russell Williams <Russ@RussWill.com>
2017-10-27 18:42:28 -0500

    I try to do it command line style

--- string1.py
+++ string1.py
@@ -38,8 +38,10 @@ def donuts(count):
 # so 'spring' yields 'spng'. However, if the string length
 # is less than 2, return instead the empty string.
 def both_ends(s):
-  # +++your code here+++
-  return
+  if len(s) >= 2:
+    return s[:2] + s[-2:]
+  else:
+    return ''
 
 
 # C. fix_start
